d4f5924d60798d4075381a658761c8f23e9ff238
xuatz <xzlow10@gmail.com>
2024-06-04 20:55:37 +0900

    docs: add curious repos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,12 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>{data.body}</p>
-
-
+<p>I've been working as a software developer for {data.lengthOfCareerSoFar} now.</p>
+<p>
+	Some of the git repos I'm quite curious/interested about are:
+	<li>
+		{#each data.reposThatImInterestedIn as repo}
+			<ul>{repo}</ul>
+		{/each}
+	</li>
+</p>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,15 @@
 import { formatDistanceToNow } from 'date-fns';
 
 export function load() {
+	const lengthOfCareerSoFar = formatDistanceToNow(new Date(2014, 2, 1));
+	const reposThatImInterestedIn = [
+		'https://github.com/pocketbase/pocketbase',
+		'https://github.com/nhost/nhost',
+	]
+
+
 	return {
-		body: `I've been working as a software developer for ${formatDistanceToNow(
-			new Date(2014, 2, 1)
-		)} now.`
+		lengthOfCareerSoFar,
+		reposThatImInterestedIn,
 	};
 }
