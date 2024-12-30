af61b1557c9e95ee351c7082f97a0bfa6da05c41
xuatz <xzlow10@gmail.com>
2024-12-31 03:35:58 +0900

    chore: run prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,23 @@
 <script>
-	/** @type {import('./$types').PageData} */
-	export let data;
+  /** @type {import('./$types').PageData} */
+  export let data;
 </script>
 
 <p>I've been working as a software developer for {data.career.duration} now.</p>
 <p>In Japan for {data.japan.duration}!</p>
 <div>
-	Some of the git repos I'm quite curious/interested about are:
-	<ul>
-		{#each data.reposThatImInterestedIn as repo}
-			<li>{repo}</li>
-		{/each}	
-	</ul>
+  Some of the git repos I'm quite curious/interested about are:
+  <ul>
+    {#each data.reposThatImInterestedIn as repo}
+      <li>{repo}</li>
+    {/each}
+  </ul>
 </div>
 <div>
-	My wishlist ^^
-	<ul>
-		{#each data.wishlist as item}
-			<li>{item}</li>
-		{/each}	
-	</ul>
+  My wishlist ^^
+  <ul>
+    {#each data.wishlist as item}
+      <li>{item}</li>
+    {/each}
+  </ul>
 </div>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,32 +1,32 @@
 import { formatDistanceToNow } from 'date-fns';
 
 export function load() {
-	const reposThatImInterestedIn = [
-		'https://github.com/pocketbase/pocketbase',
-		'https://github.com/nhost/nhost',
-	]
-	const wishlist = [
-		'Samsung Galaxy Z Fold 6',
-		'SSD NAS',
-		'Corne Keyboard',
-		'New backpack',
-		'New checked luggage',
-		'Insta360',
-	]
+  const reposThatImInterestedIn = [
+    'https://github.com/pocketbase/pocketbase',
+    'https://github.com/nhost/nhost'
+  ];
+  const wishlist = [
+    'Samsung Galaxy Z Fold 6',
+    'SSD NAS',
+    'Corne Keyboard',
+    'New backpack',
+    'New checked luggage',
+    'Insta360'
+  ];
 
-	const firstDayOfWork = new Date(2014, 2, 1);
+  const firstDayOfWork = new Date(2014, 2, 1);
 
-	const firstDayOfArrivalInJapan = new Date(2017, 11, 14)
+  const firstDayOfArrivalInJapan = new Date(2017, 11, 14);
 
-	return {
-		career: {
-			duration: formatDistanceToNow(firstDayOfWork),
-		},
-		japan: {
-			arrival: firstDayOfArrivalInJapan,
-			duration: formatDistanceToNow(firstDayOfArrivalInJapan)
-		},
-		reposThatImInterestedIn,
-		wishlist,
-	};
+  return {
+    career: {
+      duration: formatDistanceToNow(firstDayOfWork)
+    },
+    japan: {
+      arrival: firstDayOfArrivalInJapan,
+      duration: formatDistanceToNow(firstDayOfArrivalInJapan)
+    },
+    reposThatImInterestedIn,
+    wishlist
+  };
 }
